75dca9b06fbdc1bfb964b6af48bd7c3a5c3ec8ee
Utkarsh Singh <utkarshsingh.0902@gmail.com>
2025-07-03 11:41:01 +0000

    variables commit

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=12322
+let accountemail="raj@gmail.com"
+var accoutPassword="123"
+accountcity="Surat"
+let accountstate;
+//console.table([accountId,accountemail,accoutPassword,accountcity])
+
+// js can declare without assigning data type 
+// ex:- accountcity.
+console.log(accountstate) // op:- undefined.
+
